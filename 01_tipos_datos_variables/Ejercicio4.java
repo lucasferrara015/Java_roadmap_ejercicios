35b49c068338dad344017fbfde44aa0e8ecf3164
lucasferrara015 <luca015ferrara@gmail.com>
2026-06-15 20:18:38 -0300

    Create Ejercicio4.java

---
+++ 01_tipos_datos_variables/Ejercicio4.java
@@ -0,0 +1,21 @@
+// Ejercicio 4: Conversiones (Casting)
+// Módulo: Tipos de datos y variables
+// práctica personal
+
+package tipos_datos_variables;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        
+        // Declaración de un double
+        double valorDouble = 15.87;
+        
+        // Casting explícito a int
+        int valorInt = (int) valorDouble;
+        
+        // Mostrar resultados
+        System.out.println("Valor double: " + valorDouble);
+        System.out.println("Valor int (casting): " + valorInt);
+    }
+}
