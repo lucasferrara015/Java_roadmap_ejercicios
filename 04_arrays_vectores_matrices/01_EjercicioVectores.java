8d2a5538cc9ed15a20f0745d925558ed5b2a6ac4
lucasferrara015 <luca015ferrara@gmail.com>
2026-06-15 22:06:06 -0300

    Create 01_EjercicioVectores.java

---
+++ 04_arrays_vectores_matrices/01_EjercicioVectores.java
@@ -0,0 +1,37 @@
+// Ejercicio 1: Vector de nombres de usuarios
+// Módulo: Arrays, Vectores y Matrices
+// Contexto: Banco - lista de clientes
+// Visto en: Curso TodoCodeAcademy + práctica personal
+
+package arrays_vectores_matrices;
+
+import java.util.Scanner;
+
+public class EjercicioVectores1 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("¿Cuántos clientes desea ingresar? ");
+        int cantidad = sc.nextInt();
+        sc.nextLine(); // limpiar salto de línea
+        
+        // Declarar vector de Strings
+        String[] clientes = new String[cantidad];
+        
+        // Cargar nombres en el vector
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Ingrese el nombre del cliente N° " + (i+1) + ": ");
+            clientes[i] = sc.nextLine();
+        }
+        
+        // Mostrar nombres uno por uno
+        System.out.println("\nLista de clientes ingresados:");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Cliente N° " + (i+1) + ": " + clientes[i]);
+        }
+        
+        sc.close();
+    }
+}
